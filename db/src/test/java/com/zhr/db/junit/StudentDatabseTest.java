3a1e1cf00182dbddc60e39d2b65727a26995e243
ZHR-BLACK <1271035116@qq.com>
2023-06-13 14:56:21 +0800

    pom调整

--- db/src/test/java/com/zhr/db/junit/StudentDatabseTest.java
+++ db/src/test/java/com/zhr/db/junit/StudentDatabseTest.java
@@ -20,7 +20,7 @@ import java.util.List;
  */
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
-@PropertySource({"classpath:application.properties"})
+//@PropertySource({"classpath:application.properties"})
 @Slf4j
 public class StudentDatabseTest {
 
